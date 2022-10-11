0356ab370fc965f1f6d1ec61c2f4568ae438a862
Kushagra <kush12092001@gmail.com>
2022-10-12 01:56:52 +0530

    server setup

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+app.use(cors());
+
+// ROUTES
+app.get("/tasks", (req, res) => {
+  res.send("list of all tasks");
+});
+
+app.get("/addTask", (req, res) => {
+  res.send("you can add task");
+});
+
+app.get("/deleteTask", (req, res) => {
+  res.send("deleted task");
+});
+
+app.listen(8080, () => {
+  console.log("running on port 8080");
+});
